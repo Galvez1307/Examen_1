d89b3ba5fa635bc3f423a68aa07c88c0a08d367c
Emanuel Galvez <egalvez2140@gmail.com>
2024-11-04 20:38:08 -0600

    4to Avance

--- lib/app_router.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-// Importa los archivos de los widgets de Noticias y Lista de Tareas cuando los crees
-// import 'noticias_widget.dart';
-// import 'lista_tareas_widget.dart';
---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+
+  ],
+);
+
